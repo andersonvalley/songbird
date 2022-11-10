c58f61454f543768dd0b5ad318e2bb8134ae5957
Andrey Mironov <andreesssen@gmail.com>
2022-11-10 13:12:01 +0300

    feat: make desktop layout

--- SongBird/src/js/main.js
+++ SongBird/src/js/main.js
@@ -1,2 +1,17 @@
 import '../index.html';
 import '../scss/main.scss';
+
+function rangeInput(input, shadow) {
+  const inputRange = window.document.querySelector(input);
+  const inputShadow = window.document.querySelector(shadow);
+
+  inputRange.addEventListener('input', (e) => {
+    if (e.target.value < 30 && e.target.value >= 1) {
+      inputShadow.style.width = `${+e.target.value + 1}%`;
+    } else {
+      inputShadow.style.width = `${e.target.value}%`;
+    }
+  });
+}
+
+rangeInput('.track__line', '.track__line_back');
